6740f7e4c84a9ebb5fb215080576583bcce680bf
morrapablo <morrapablo@271561b7-901e-0410-bbad-b7b006ef6797>
2006-10-03 17:01:54 +0000

    saque los tildes de las palabras --> cafe

--- Dieta/src/edu/utn/frba/ia/grupo7/ag/Desayuno.java
+++ Dieta/src/edu/utn/frba/ia/grupo7/ag/Desayuno.java
@@ -4,27 +4,27 @@ import org.jgap.RandomGenerator;
 
 public enum Desayuno implements Comida {
 
-	DESAYUNO_0("Café con leche y medialunas de grasa", 443),
-	DESAYUNO_1("Café con leche y tostadas", 368),
-	DESAYUNO_2("Café con leche y tostadas con manteca", 390),
-	DESAYUNO_3("Café con leche y galletas de salvado", 350),
+	DESAYUNO_0("Cafe con leche y medialunas de grasa", 443),
+	DESAYUNO_1("Cafe con leche y tostadas", 368),
+	DESAYUNO_2("Cafe con leche y tostadas con manteca", 390),
+	DESAYUNO_3("Cafe con leche y galletas de salvado", 350),
 	DESAYUNO_4("Mate cocido y medialunas de grasa", 180),
 	DESAYUNO_5("Mate cocido y tostadas", 84),
 	DESAYUNO_6("Mate cocido y tostadas con manteca", 187),
 	DESAYUNO_7("Mate cocido y galletas de salvado", 135),
-	DESAYUNO_8("Té y medialunas de grasa", 180),
-	DESAYUNO_9("Té y tostadas", 84),
-	DESAYUNO_10("Té y tostadas con manteca", 168),
-	DESAYUNO_11("Té y galletas de salvado", 110),
-	DESAYUNO_12("Té con leche y medialunas de grasa", 210),
-	DESAYUNO_13("Té con leche y tostadas", 90),
-	DESAYUNO_14("Té con leche y tostadas con manteca", 187),
-	DESAYUNO_15("Té con leche y galletas de salvado", 135),
+	DESAYUNO_8("Te y medialunas de grasa", 180),
+	DESAYUNO_9("Te y tostadas", 84),
+	DESAYUNO_10("Te y tostadas con manteca", 168),
+	DESAYUNO_11("Te y galletas de salvado", 110),
+	DESAYUNO_12("Te con leche y medialunas de grasa", 210),
+	DESAYUNO_13("Te con leche y tostadas", 90),
+	DESAYUNO_14("Te con leche y tostadas con manteca", 187),
+	DESAYUNO_15("Te con leche y galletas de salvado", 135),
 	DESAYUNO_16("Chocolatada con churros", 520),
 	DESAYUNO_17("Leche con cereales", 580),
 	DESAYUNO_18("Yogur y 2 tostadas", 440),
-	DESAYUNO_19("Café con leche, 1 medialuna y 1 naranja", 520),
-	DESAYUNO_20("Café con leche, 1 naranja, 1 manzana", 510)
+	DESAYUNO_19("Cafe con leche, 1 medialuna y 1 naranja", 520),
+	DESAYUNO_20("Cafe con leche, 1 naranja, 1 manzana", 510)
 	;
 	
 	private String nombre;
