3530f4dc99ff07b623fa379e15af7af2d1be2809
morrapablo <morrapablo@271561b7-901e-0410-bbad-b7b006ef6797>
2006-10-03 06:34:01 +0000

    fin?

--- trunk/Dieta/src/edu/utn/frba/ia/grupo7/ag/Dieta.java
+++ trunk/Dieta/src/edu/utn/frba/ia/grupo7/ag/Dieta.java
@@ -16,19 +16,23 @@ public class Dieta {
 	/*
 	 * Cantidad de veces que evoluciona la poblacion
 	 */
-	private static int MAX_GENERACIONES = 100;
+	private static final int MAX_GENERACIONES = 100;
 	
 	/*
 	 * Cantidad de individuos en la poblacion
 	 */
-	private static int POBLACION = 500;
+	private static final int POBLACION = 500;
 	
 	/*
 	 * Cantidad de dias que tiene la dieta
 	 */
-	private static int DIAS_DIETA = 7;
+	private static final int DIAS_DIETA = 7;
 	
-	
+	/*
+	 * Maximo de calorias diarias
+	 */
+	private static final int MAX_CALORIAS_DIARIAS = 1300;
+
 	
 	/**
 	 * Programa principal
@@ -36,10 +40,14 @@ public class Dieta {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		int calorias = leerCalorias(args);
+		
 		Configuration conf = new DefaultConfiguration();
 		
 		// Inicializar la funcion de aptitud
 		FuncionAptitud funcionAptitud = new FuncionAptitud();
+		funcionAptitud.setMaxCaloriasDiarias(calorias);
 		
 		// Generar poblacion inicial
 		Genotype poblacion = null;
@@ -53,18 +61,24 @@ public class Dieta {
 		// mutacion a lo largo de MAX_GENERACIONES generaciones
 		poblacion.evolve(MAX_GENERACIONES);
 
-//		for(Object cromo : poblacion.getFittestChromosomes(2)) {
-//			IChromosome cromosoma = (IChromosome) cromo;
-//			System.out.println(cromosoma.getFitnessValue());
-//			mostrarDieta(cromosoma);
-//		}
-		
 		// Obtener el mejor individuo de la generacion actual
 		IChromosome solucion = poblacion.getFittestChromosome();
 
 		// Imprimir solucion obtenida
 		mostrarDieta(solucion);
 		
+	}
+
+	private static int leerCalorias(String[] args) {
+		Integer calorias = null;
+		try {
+			calorias = new Integer(args[0]);
+		} catch (Exception e) {
+			// No se interpreto el resultado - utilizar maximo default
+			calorias = MAX_CALORIAS_DIARIAS;
+		}
+		System.out.println("Generando dieta de " + calorias + " kcal diarias...\n");
+		return calorias;
 	}
 
 	/**
@@ -108,10 +122,19 @@ public class Dieta {
 	 */
 	private static void mostrarDieta(IChromosome solucion) {
 		
+		int kcalDia = 0;
+		int kcalSuma = 0;
+		
 		for (int i=0; i < DIAS_DIETA; i++) {
-			System.out.println("Dia " + (i+1) + " (" + ((Dia)solucion.getGene(i)).getKcal() + "): \n" + 
+			
+			kcalDia = ((Dia)solucion.getGene(i)).getKcal();
+			kcalSuma = kcalSuma + kcalDia;
+			
+			System.out.println("Dia " + (i+1) + " (" + kcalDia + "): \n" + 
 					solucion.getGene(i).toString());
 		}
+		
+		System.out.println("Promedio Kcal. diario: " + kcalSuma / DIAS_DIETA + "\n");
 	}
 
 }
--- trunk/Dieta/src/edu/utn/frba/ia/grupo7/ag/FuncionAptitud.java
+++ trunk/Dieta/src/edu/utn/frba/ia/grupo7/ag/FuncionAptitud.java
@@ -3,6 +3,9 @@
  */
 package edu.utn.frba.ia.grupo7.ag;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jgap.FitnessFunction;
 import org.jgap.Gene;
 import org.jgap.IChromosome;
@@ -13,25 +16,72 @@ import org.jgap.IChromosome;
 public class FuncionAptitud extends FitnessFunction {
 
 	private static final long serialVersionUID = 1L;
-	private static final int MAX_CALORIAS_DIARIAS = 1000;
+	
+	private Integer maxCaloriasDiarias = 1500;
 
 	/* (non-Javadoc)
 	 * @see org.jgap.FitnessFunction#evaluate(org.jgap.IChromosome)
 	 */
 	@Override
 	protected double evaluate(IChromosome individuo) {
-		double factorCalorias = 0;
 		double factorRepeticion = 0;
+		double factorCalorias = 0;
+		Map<Comida, Integer> contador = new HashMap<Comida, Integer>();
 		
 		int calorias = 0;
-		for (Gene dia : individuo.getGenes()) {
-			calorias += ((Dia)dia).getKcal(); 
+		int cantDiasExceso = 0;
+		for (Gene gen : individuo.getGenes()) {
+			
+			// Acumular las calorias
+			Dia dia = (Dia) gen;
+			calorias += dia.getKcal();
+			
+			// Registrar si es un dia con exceso de calorias
+			cantDiasExceso += (dia.getKcal() > this.maxCaloriasDiarias) ? 1 : 0;
+			
+			// Registrar la repeticion de comidas
+			Comida[] comidas = (Comida[]) dia.getAllele();
+			for (Comida comida : comidas) {
+				if (contador.containsKey(comida)) {
+					contador.put(comida, contador.get(comida) + 1);
+				} else {
+					contador.put(comida, 1);
+				}
+			}
 		}
+
+		// --------- Calcular puntos por calorias ---------
+		// Si tengo dias con exceso de calorias, restar puntos por dia
+		factorCalorias -= (200 * cantDiasExceso);
+		
 		// Si las calorias de la dieta estan dentro de lo
-		// esperado, sumar 1000 puntos a la aptitud
-		factorCalorias = MAX_CALORIAS_DIARIAS * 7 < calorias ? 1000 : 0;
-				
-		return factorCalorias + factorRepeticion;
+		// esperado, sumar puntos a la aptitud
+		if (this.maxCaloriasDiarias * 7 <= calorias) {
+			factorCalorias +=  100;
+			
+		// En otro caso, restar puntos
+		} else {
+			factorCalorias -= 100;
+		}
+
+		
+		// ----- Calcular puntos por repeticion de comidas -------
+		// El que tiene mas comidas es el que menos repite
+		factorRepeticion = contador.size() * 200;
+
+		// ----- Calcular la puntuacion final -------
+		double puntuacion = factorCalorias + factorRepeticion;
+		
+		// Si la puntuacion final es negativa, devolver cero
+		return (puntuacion >= 0) ? puntuacion : 0;
+	}
+
+	public Integer getMaxCaloriasDiarias() {
+		return maxCaloriasDiarias;
+	}
+
+	public void setMaxCaloriasDiarias(Integer maxCaloriasDiarias) {
+		this.maxCaloriasDiarias = maxCaloriasDiarias;
 	}
 
 }
